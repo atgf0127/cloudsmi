88ccaee29920533bbff562d6e6e342b116a5ae4d
x_ai <atgf0127@gmail.com>
2018-09-13 00:41:47 +0800

    ch

---
+++ domain.js
@@ -0,0 +1,7 @@
+switch(location.host){
+case 'www.cloudsmi.com.htm'/*tpa=http://www.cloudsmi.com/www.cloudsmi.com*/:
+location.href="index-1.htm"/*tpa=http://www.cloudsmi.com/company/*/
+case 'cloudsmi.com.htm'/*tpa=http://www.cloudsmi.com/cloudsmi.com*/:
+location.href="index-1.htm"/*tpa=http://www.cloudsmi.com/company/*/
+break;
+}
